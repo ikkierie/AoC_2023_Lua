2d42eec57b1b04b2e19c06f99cb9427e91731bea
ikkierie <ikkierie@protonmail.com>
2023-12-09 10:50:52 +0000

    Day 9

---
+++ 09/b.lua
@@ -0,0 +1,20 @@
+local func   = require "func"
+local seq    = require "seq"
+local stream = require "stream"
+
+function extrapolate(xs)
+    local diffs = stream.from.list(xs)
+        :windows(2)
+        :map__u(func.meta.sub:flip())
+        :collect(seq)
+    return (diffs:all(func.meta.eq:bind(0)))
+        and xs[1]
+        or  xs[1] - extrapolate(diffs)
+end
+
+local sum = 0
+for line in io.lines([[input.txt]]) do
+    local xs = seq.from.string(line, "%s+"):map(tonumber)
+    sum = sum + extrapolate(xs)
+end
+print(sum)
