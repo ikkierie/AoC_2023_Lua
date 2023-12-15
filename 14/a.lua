73594ed6aa599a9a4213943aa1923c023955f308
ikkierie <ikkierie@protonmail.com>
2023-12-15 14:06:45 +0000

    [WIP] Day 14 a

--- 14/a.lua
+++ 14/a.lua
@@ -3,40 +3,31 @@ require "utils"
 
 local grid = seq.from.lines([[input.txt]]):apply(seq.from.string)
 
+local function display(x, y, colour)
+    io.write("\x1b[2J\x1b[H")
+    print(x, y)
+    local before = grid[y][x]
+    grid[y][x] = "\x1b[" .. (colour or 0) .. "m0\x1b[m"
+    print(grid:map(seq.concat):concat("\n"))
+    grid[y][x] = before
+    io.read()
+end
+
 local score = 0
 for x = 1, #grid[1] do
     for y = 1, #grid do
-        local row   = grid[y]
-        local piece = row[x]
+        local piece = grid[y][x]
         if piece == "O" then
-            -- io.write("\x1b[2J\x1b[H")
-            -- print(x, y)
-            -- local before = grid[y][x]
-            -- grid[y][x] = "\x1b[93m0\x1b[m"
-            -- print(grid:map(seq.concat):concat("\n"))
-            -- grid[y][x] = before
-            -- io.read()
-            row[x] = "."
+            -- display(x, y, 93)
+            grid[y][x] = "."
             while y > 1 do
-                -- io.write("\x1b[2J\x1b[H")
-                -- print(x, y)
-                -- local before = grid[y][x]
-                -- grid[y][x] = "\x1b[91m0\x1b[m"
-                -- print(grid:map(seq.concat):concat("\n"))
-                -- grid[y][x] = before
-                -- io.read()
-                if grid[y - 1] and grid[y - 1][x] ~= "." then
+                -- display(x, y, 91)
+                if grid[y - 1] and (grid[y - 1][x] ~= ".") then
                     break
                 end
                 y = y - 1
             end
-            -- io.write("\x1b[2J\x1b[H")
-            -- print(x, y)
-            -- local before = grid[y][x]
-            -- grid[y][x] = "\x1b[92m0\x1b[m"
-            -- print(grid:map(seq.concat):concat("\n"))
-            -- grid[y][x] = before
-            -- io.read()
+            -- display(x, y, 92)
             grid[y][x] = piece
             score      = score - y + #grid + 1
         end
